46ec53cc6fed8efb85bd18a218421eed4b4ef8cc
Simiams <simon.convert@epsi.fr>
2023-10-03 15:30:07 +0200

    first commit

---
+++ back/src/main/java/com/team/naostory/NaostoryApplication.java
@@ -0,0 +1,13 @@
+package com.team.naostory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaostoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaostoryApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/team/naostory/NaostoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.naostory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaostoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
